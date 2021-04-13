229f74c17857e12003d460b0fd6f551916f7a7cc
david <tardis@yeah.net>
2021-04-13 20:59:47 +0800

    更新注释说明

--- profileReset.php
+++ profileReset.php
@@ -2,14 +2,15 @@
 require __DIR__ . '/Curl.php';
 require __DIR__ . '/config.php';
 
-//可以通过本接口重置浏览器环境（指纹、配置信息、所有浏览器数据）
+//可以通过本接口重置浏览器环境（指纹、配置信息、所有浏览器数据），如果只是更改配置文件参数（比如代理IP），请使用update接口
 //本接口字段和profileCreate接口参数相同。
 //调用本接口会重置硬件指纹
 //可以通过本接口修改指定的环境参数
 $req = array(
     'model' => array(
-        'profileId' => '210109154430835001',//配置文件ID必传
-        'userAgent' => 'new user agent'//如果指定了环境参数，则会进行修改，如果不传则继续使用之前的值
+        'profileId' => '210406144528544013',//配置文件ID必传
+        'userAgent' => 'new user agent',//如果指定了环境参数，则会进行修改，如果不传则继续使用之前的值
+        'proxyFlag'=>false
     ),
 );
 
